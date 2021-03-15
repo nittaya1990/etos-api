1a94dd5e418a4103512d39def30b390c8941ac78
Tobias Persson <tobiaspn@axis.com>
2021-03-15 09:20:41 +0100

    Make aiohttp use the environment variables by default (#17)

--- src/etos_api/library/graphql.py
+++ src/etos_api/library/graphql.py
@@ -31,6 +31,7 @@ class GraphqlQueryHandler:  # pylint:disable=too-few-public-methods
         self.transport = AIOHTTPTransport(
             url=self.etos.debug.graphql_server,
             timeout=self.etos.debug.default_http_timeout,
+            client_session_args={"trust_env": True}
         )
 
     async def execute(self, query):
