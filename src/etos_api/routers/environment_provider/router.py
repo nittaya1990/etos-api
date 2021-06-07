c6e23510f7af07dd5f09e6d24f0f726e8f256ef3
Tobias Persson <tobiaspn@axis.com>
2021-06-07 13:02:37 +0200

    Verify environment provider configuration before continuing (#19)

--- src/etos_api/routers/environment_provider/router.py
+++ src/etos_api/routers/environment_provider/router.py
@@ -28,6 +28,53 @@ ROUTER = APIRouter()
 LOGGER = logging.getLogger(__name__)
 
 
+async def _wait_for_configuration(etos_library, environment):
+    """Wait for the environment provider configuration to apply.
+
+    :param etos_library: An ETOS library instance for requesting the environment provider.
+    :type etos_library: :obj:`etos_lib.ETOS`
+    :param environment: Environment that has been configured.
+    :type environment: :obj:`etos_api.routers.etos.schemas.ConfigureEnvironmentProviderRequest`
+    """
+    LOGGER.info("Waiting for configuration to be applied in the environment provider.")
+    end_time = time.time() + etos_library.debug.default_http_timeout
+    LOGGER.debug("Timeout: %r", etos_library.debug.default_http_timeout)
+    async with aiohttp.ClientSession() as session:
+        while time.time() < end_time:
+            try:
+                async with session.get(
+                    f"{etos_library.debug.environment_provider}/configure",
+                    params={"suite_id": environment.suite_id},
+                    headers={
+                        "Content-Type": "application/json",
+                        "Accept": "application/json",
+                    },
+                ) as response:
+                    assert 200 <= response.status < 400
+                    response_json = await response.json()
+                    LOGGER.info("Configuration: %r", response_json)
+                    assert response_json.get("dataset") is not None
+                    assert response_json.get("iut_provider") is not None
+                    assert response_json.get("log_area_provider") is not None
+                    assert response_json.get("execution_space_provider") is not None
+                break
+            except AssertionError:
+                if response.status < 400:
+                    LOGGER.warning("Configuration not ready yet.")
+                else:
+                    LOGGER.warning(
+                        "Configuration verification request failed: %r, %r",
+                        response.status,
+                        response.reason,
+                    )
+                await asyncio.sleep(2)
+        else:
+            raise HTTPException(
+                status_code=400,
+                detail="Environment provider configuration did not apply properly",
+            )
+
+
 @ROUTER.post(
     "/environment_provider/configure", tags=["environment_provider"], status_code=204
 )
@@ -70,3 +117,4 @@ async def configure_environment_provider(
                 status_code=400,
                 detail=f"Unable to configure environment provider with '{environment.json()}'",
             )
+        await _wait_for_configuration(etos_library, environment)
